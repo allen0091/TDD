c30e0d9757f932139bf8ca4a6d4926cf97f7c4c9
Allen00991 <allen00991.cs10@nycu.edu.tw>
2022-05-16 12:47:42 +0000

    refactor test function

--- BookStore_test.go
+++ BookStore_test.go
@@ -22,9 +22,53 @@ func TestBuyTwoDifferentBook(t *testing.T) {
 
 }
 
+func Test_calculatePrice(t *testing.T) {
+	type args struct {
+		books []int
+	}
+	tests := []struct {
+		name string // test name
+		args args
+		want float32 // expected value
+	}{
+		{
+			name: "Basic : zero book",
+			args: args{books: []int{1}},
+			want: 8,
+		},
+		{
+			name: "Basic : Ep1",
+			args: args{books: []int{1}},
+			want: 8,
+		},
+		{
+			name: "SimpleDiscounts : two different book",
+			args: args{books: []int{1, 2}},
+			want: 2 * 8 * 0.95,
+		},
+		{
+			name: "SimpleDiscounts : three different book",
+			args: args{books: []int{1, 2, 4}},
+			want: 3 * 8 * 0.95,
+		},
+		{
+			name: "SimpleDiscounts : five different book",
+			args: args{books: []int{1, 2, 3, 4, 5}},
+			want: 5 * 8 * 0.95,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := calculatePrice(tt.args.books); got != tt.want {
+				t.Errorf("calculatePrice() should got %v, but got %v", tt.want, got)
+			}
+		})
+	}
+}
+
 func calculatePrice(books []int) float32 {
 	count := len(books)
-	if count == 2 {
+	if count > 1 {
 		return float32(count) * 8 * 0.95
 	}
 	return 8
