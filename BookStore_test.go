f4482bb924cff24bb4c873129b0911bd2ca5e08f
Allen00991 <allen00991.cs10@nycu.edu.tw>
2022-05-16 12:11:16 +0000

    test1 pass

--- BookStore_test.go
+++ BookStore_test.go
@@ -4,10 +4,14 @@ import "testing"
 
 func TestBuyOneBook(t *testing.T) {
 
-	price := calculatePrice([]int{1}) // 還沒有這個 Function
+	price := calculatePrice([]int{1})
 
 	if price != 8 {
 		t.Fatal("Price should be 8, but got ", price)
 	}
 
+}
+
+func calculatePrice(books []int) float32 {
+	return 8
 }
